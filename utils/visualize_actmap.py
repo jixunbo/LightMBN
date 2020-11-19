cf9f948e052cc2d15e0dc3964a04e9d7c0c360b3
Ji <x.ji@tum.de>
2020-11-19 21:00:05 +0100

    Modify data preparation

---
+++ utils/visualize_actmap.py
@@ -0,0 +1,273 @@
+"""Visualizes CNN activation maps to see where the CNN focuses on to extract features.
+Reference:
+    - Zagoruyko and Komodakis. Paying more attention to attention: Improving the
+      performance of convolutional neural networks via attention transfer. ICLR, 2017
+    - Zhou et al. Omni-Scale Feature Learning for Person Re-Identification. ICCV, 2019.
+"""
+
+import sys
+import os
+import numpy as np
+import os.path as osp
+import cv2
+import torch
+from torch.nn import functional as F
+
+sys.path.append(osp.join(osp.abspath(osp.dirname(__file__)), ".."))
+
+import data_v2
+import loss
+import optim
+from model import Model
+from option import args
+import utils.utility as utility
+from utils.model_complexity import compute_model_complexity
+import yaml
+
+
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
+GRID_SPACING = 10
+
+
+@torch.no_grad()
+def visactmap(
+    model,
+    test_loader,
+    save_dir,
+    width,
+    height,
+    use_gpu,
+    img_mean=None,
+    img_std=None
+):
+    if img_mean is None or img_std is None:
+        # use imagenet mean and std
+        img_mean = IMAGENET_MEAN
+        img_std = IMAGENET_STD
+
+    model.eval()
+
+    for target in list(test_loader.keys()):
+        data_loader = test_loader[target]['query']  # only process query images
+        # original images and activation maps are saved individually
+        actmap_dir = osp.join(save_dir, 'actmap_' + target)
+
+        if not osp.exists(actmap_dir):
+            os.makedirs(actmap_dir)
+
+        print('Visualizing activation maps for {} ...'.format(target))
+
+        for batch_idx, data in enumerate(data_loader):
+
+            # if batch_idx == 10:
+            #     break
+
+            imgs, paths = data[0], data[-1]
+            if use_gpu:
+                imgs = imgs.cuda()
+
+            # forward to get convolutional feature maps
+            try:
+                outputs_list = model(imgs)
+            except TypeError:
+                raise TypeError(
+                    'forward() got unexpected keyword argument "return_featuremaps". '
+                    'Please add return_featuremaps as an input argument to forward(). When '
+                    'return_featuremaps=True, return feature maps only.'
+                )
+            # print(len(outputs_list))
+            # # if batch_idx == 2:
+            # print(outputs_list[0].size())
+
+            #     break
+
+            for j, output in enumerate(zip(*outputs_list)):
+                # if outputs.dim() != 4:
+                #     raise ValueError(
+                #         'The model output is supposed to have '
+                #         'shape of (b, c, h, w), i.e. 4 dimensions, but got {} dimensions. '
+                #         'Please make sure you set the model output at eval mode '
+                #         'to be the last convolutional feature maps'.format(
+                #             outputs.dim()
+                #         )
+                #     )
+                n_fmaps = len(output)
+                # print(n_fmaps)/
+                grid_img = 255 * np.ones(
+                    (height, (n_fmaps + 1) * width + n_fmaps * GRID_SPACING, 3), dtype=np.uint8
+                )
+                # RGB image
+                if use_gpu:
+                    imgs = imgs.cpu()
+                img = imgs[j, ...]
+                for t, m, s in zip(img, img_mean, img_std):
+                    t.mul_(s).add_(m).clamp_(0, 1)
+                img_np = np.uint8(np.floor(img.numpy() * 255))
+                # (c, h, w) -> (h, w, c)
+                img_np = img_np.transpose((1, 2, 0))
+
+
+                path = paths[j]
+                imname = osp.basename(osp.splitext(path)[0])
+                # print(imname[:4])
+                # print()
+                if imname[:4] not in ['4117','4699','0580','0601']:
+                    continue
+
+                # compute activation maps
+                for output_number, outputs in enumerate(output):
+
+                    # outputs = (outputs**2).sum(1)
+                    # b, h, w = outputs.size()
+                    # outputs = outputs.view(b, h * w)
+                    # outputs = F.normalize(outputs, p=2, dim=1)
+                    # outputs = outputs.view(b, h, w)
+                    # print(outputs.size())
+                    outputs = (outputs**2).sum(0)
+                    h, w = outputs.size()
+                    outputs = outputs.view(h * w)
+                    outputs = F.normalize(outputs, p=2, dim=0)
+                    outputs = outputs.view(h, w)
+
+                    if outputs.size()[0]!=24:
+                        z=torch.zeros(12,8).cuda()
+                        if output_number ==3:
+                            outputs=torch.cat((outputs,z),0) 
+                        if output_number == 4 :
+                            outputs=torch.cat((z,outputs),0) 
+                    # if outputs.size()[0]!=24:
+                    #     z=torch.zeros(8,8)
+                    #     if output_number ==1:
+                    #         outputs=torch.cat((outputs,z,z),0) 
+                    #     if output_number == 2 :
+                    #         outputs=torch.cat((z,outputs,z),0) 
+                    #     if output_number == 3 :
+                    #         outputs=torch.cat((outputs,z,z),0) 
+
+                    if use_gpu:
+                        outputs = outputs.cpu()
+
+                    # for j in range(outputs.size(0)):
+                        # get image name
+                        # print(j)
+                    path = paths[j]
+                    imname = osp.basename(osp.splitext(path)[0])
+
+
+
+                    # activation map
+                    # am = outputs[j, ...].numpy()
+                    am = outputs.numpy()
+                    # print(output_number)
+                    # print(am)
+                    try:
+                        # if output_number>=1:
+                        #     am = cv2.resize(am,(width,height/2))
+                        # else:
+                        #     am = cv2.resize(am, (width, height))
+                        am = cv2.resize(am,(width,height))
+                    except:
+                        print(path)
+                        break
+
+                    am = 255 * (am - np.min(am)) / (
+                        np.max(am) - np.min(am) + 1e-12
+                    )
+                    am = np.uint8(np.floor(am))
+                    am = cv2.applyColorMap(am, cv2.COLORMAP_JET)
+
+                    # overlapped
+                    overlapped = img_np * 0.45 + am * 0.55
+                    overlapped[overlapped > 255] = 255
+                    overlapped = overlapped.astype(np.uint8)
+
+                    # save images in a single figure (add white spacing between images)
+                    # from left to right: original image, activation map, overlapped image
+
+                    grid_img[:, :width, :] = img_np[:, :, ::-1]
+                    grid_img[:,
+                             (output_number + 1) * width + (output_number + 1) * GRID_SPACING:(output_number + 2) * width + (output_number + 1) * GRID_SPACING, :] = overlapped
+                    # grid_img[:, 2 * width + 2 * GRID_SPACING:, :] = overlapped
+                cv2.imwrite(osp.join(actmap_dir, imname  + '.jpg'), grid_img)
+
+            if (batch_idx + 1) % 10 == 0:
+                print(
+                    '- done batch {}/{}'.format(
+                        batch_idx + 1, len(data_loader)
+                    )
+                )
+
+
+def main():
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--root', type=str)
+    # parser.add_argument('-d', '--dataset', type=str, default='market1501')
+    # parser.add_argument('-m', '--model', type=str, default='osnet_x1_0')
+    # parser.add_argument('--weights', type=str)
+    # parser.add_argument('--save-dir', type=str, default='log')
+    # parser.add_argument('--height', type=int, default=256)
+    # parser.add_argument('--width', type=int, default=128)
+    # args = parser.parse_args()
+
+    if args.config != '':
+        with open(args.config, 'r') as f:
+            config = yaml.load(f)
+        for op in config:
+            setattr(args, op, config[op])
+
+    # loader = data.Data(args)
+    ckpt = utility.checkpoint(args)
+    loader = data_v2.ImageDataManager(args)
+    model = Model(args, ckpt)
+    optimzer = optim.make_optimizer(args, model)
+    # loss = loss.make_loss(args, ckpt) if not args.test_only else None
+
+    start = -1
+    if args.load != '':
+        start = ckpt.resume_from_checkpoint(
+            osp.join(ckpt.dir, 'model-latest.pth'), model, optimzer) - 1
+    if args.pre_train != '':
+        ckpt.load_pretrained_weights(model, args.pre_train)
+
+    scheduler = optim.make_scheduler(args, optimzer, start)
+
+    # print('[INFO] System infomation: \n {}'.format(get_pretty_env_info()))
+    ckpt.write_log('[INFO] Model parameters: {com[0]} flops: {com[1]}'.format(
+        com=compute_model_complexity(model, (1, 3, args.height, args.width))))
+
+    use_gpu = torch.cuda.is_available()
+
+    # datamanager = torchreid.data.ImageDataManager(
+    #     root=args.root,
+    #     sources=args.dataset,
+    #     height=args.height,
+    #     width=args.width,
+    #     batch_size_train=100,
+    #     batch_size_test=100,
+    #     transforms=None,
+    #     train_sampler='SequentialSampler'
+    # )
+    test_loader = loader.testloader
+
+    # model = torchreid.models.build_model(
+    #     name=args.model,
+    #     num_classes=datamanager.num_train_pids,
+    #     use_gpu=use_gpu
+    # )
+
+    if use_gpu:
+        model = model.cuda()
+
+    # if args.weights and check_isfile(args.weights):
+    #     load_pretrained_weights(model, args.weights)
+
+    save_dir = ckpt.dir
+
+    visactmap(
+        model, test_loader, save_dir, args.width, args.height, use_gpu
+    )
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/visualize_rank.py
@@ -0,0 +1,354 @@
+from __future__ import print_function, absolute_import
+import shutil
+
+import sys
+import os
+import numpy as np
+import os.path as osp
+import cv2
+import torch
+from torch.nn import functional as F
+
+sys.path.append(osp.join(osp.abspath(osp.dirname(__file__)), ".."))
+
+import data_v2
+import loss
+import optim
+from model import Model
+from option import args
+import utils.utility as utility
+from utils.model_complexity import compute_model_complexity
+import yaml
+
+
+__all__ = ['visualize_ranked_results']
+
+GRID_SPACING = 10
+QUERY_EXTRA_SPACING = 90
+BW = 5  # border width
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+
+
+def visualize_ranked_results(
+    distmat, query_loader, gallery_loader, data_type, width=128, height=256, save_dir='', topk=10
+):
+    """Visualizes ranked results.
+    Supports both image-reid and video-reid.
+    For image-reid, ranks will be plotted in a single figure. For video-reid, ranks will be
+    saved in folders each containing a tracklet.
+    Args:
+        distmat (numpy.ndarray): distance matrix of shape (num_query, num_gallery).
+        dataset (tuple): a 2-tuple containing (query, gallery), each of which contains
+            tuples of (img_path(s), pid, camid, dsetid).
+        data_type (str): "image" or "video".
+        width (int, optional): resized image width. Default is 128.
+        height (int, optional): resized image height. Default is 256.
+        save_dir (str): directory to save output images.
+        topk (int, optional): denoting top-k images in the rank list to be visualized.
+            Default is 10.
+    """
+    num_q, num_g = distmat.shape
+    print(num_q,num_g)
+    def mkdir_if_missing(s_dir):
+
+        if not osp.exists(s_dir):
+            os.makedirs(s_dir)
+
+    mkdir_if_missing(save_dir)
+    print('# query: {}\n# gallery {}'.format(num_q, num_g))
+    print('Visualizing top-{} ranks ...'.format(topk))
+
+    query = query_loader
+    gallery = gallery_loader
+    print(len(query),len(gallery))
+
+    assert num_q == len(query)
+    assert num_g == len(gallery)
+
+    indices = np.argsort(distmat, axis=1)
+
+    def _cp_img_to(src, dst, rank, prefix, matched=False):
+        """
+        Args:
+            src: image path or tuple (for vidreid)
+            dst: target directory
+            rank: int, denoting ranked position, starting from 1
+            prefix: string
+            matched: bool
+        """
+        if isinstance(src, (tuple, list)):
+            if prefix == 'gallery':
+                suffix = 'TRUE' if matched else 'FALSE'
+                dst = osp.join(
+                    dst, prefix + '_top' + str(rank).zfill(3)
+                ) + '_' + suffix
+            else:
+                dst = osp.join(dst, prefix + '_top' + str(rank).zfill(3))
+            mkdir_if_missing(dst)
+            for img_path in src:
+                shutil.copy(img_path, dst)
+        else:
+            dst = osp.join(
+                dst, prefix + '_top' + str(rank).zfill(3) + '_name_' +
+                osp.basename(src)
+            )
+            shutil.copy(src, dst)
+    num_q =200
+
+
+    for q_idx in range(num_q):
+        qimg_path, qpid, qcamid = query[q_idx][:3]
+        qimg_path_name = qimg_path[0] if isinstance(
+            qimg_path, (tuple, list)
+        ) else qimg_path
+
+        if data_type == 'image':
+            qimg = cv2.imread(qimg_path)
+            qimg = cv2.resize(qimg, (width, height))
+            qimg = cv2.copyMakeBorder(
+                qimg, BW, BW, BW, BW, cv2.BORDER_CONSTANT, value=(0, 0, 0)
+            )
+            # resize twice to ensure that the border width is consistent across images
+            qimg = cv2.resize(qimg, (width, height))
+            num_cols = topk + 1
+            grid_img = 255 * np.ones(
+                (
+                    height,
+                    num_cols * width + topk * GRID_SPACING + QUERY_EXTRA_SPACING, 3
+                ),
+                dtype=np.uint8
+            )
+            grid_img[:, :width, :] = qimg
+        else:
+            qdir = osp.join(
+                save_dir, osp.basename(osp.splitext(qimg_path_name)[0])
+            )
+            mkdir_if_missing(qdir)
+            _cp_img_to(qimg_path, qdir, rank=0, prefix='query')
+
+        rank_idx = 1
+        for g_idx in indices[q_idx, :]:
+            gimg_path, gpid, gcamid = gallery[g_idx][:3]
+            invalid = (qpid == gpid) & (qcamid == gcamid)
+
+            if not invalid:
+                matched = gpid == qpid
+                if data_type == 'image':
+                    border_color = GREEN if matched else RED
+                    gimg = cv2.imread(gimg_path)
+                    gimg = cv2.resize(gimg, (width, height))
+                    gimg = cv2.copyMakeBorder(
+                        gimg,
+                        BW,
+                        BW,
+                        BW,
+                        BW,
+                        cv2.BORDER_CONSTANT,
+                        value=border_color
+                    )
+                    gimg = cv2.resize(gimg, (width, height))
+                    start = rank_idx * width + rank_idx * GRID_SPACING + QUERY_EXTRA_SPACING
+                    end = (
+                        rank_idx + 1
+                    ) * width + rank_idx * GRID_SPACING + QUERY_EXTRA_SPACING
+                    ggname = osp.basename(osp.splitext(gimg_path)[0])
+                    
+                    font=cv2.FONT_HERSHEY_SIMPLEX#使用默认字体
+                    # cv2.putText(gimg, ggname,(0,20),font,0.6,(255,255,255),2)
+
+                    grid_img[:, start:end, :] = gimg
+                else:
+                    _cp_img_to(
+                        gimg_path,
+                        qdir,
+                        rank=rank_idx,
+                        prefix='gallery',
+                        matched=matched
+                    )
+
+                rank_idx += 1
+                if rank_idx > topk:
+                    break
+
+        if data_type == 'image':
+            imname = osp.basename(osp.splitext(qimg_path_name)[0])
+            cv2.imwrite(osp.join(save_dir, imname + '.jpg'), grid_img)
+
+        if (q_idx + 1) % 100 == 0:
+            print('- done {}/{}'.format(q_idx + 1, num_q))
+
+    print('Done. Images have been saved to "{}" ...'.format(save_dir))
+
+
+# tools for reid datamanager data_v2
+def _parse_data_for_train(data):
+    imgs = data[0]
+    pids = data[1]
+    return imgs, pids
+
+
+def _parse_data_for_eval(data):
+    imgs = data[0]
+    pids = data[1]
+    camids = data[2]
+    return imgs, pids, camids
+
+
+def extract_feature(model, device, loader, args):
+    features = torch.FloatTensor()
+    pids, camids = [], []
+
+    for d in loader:
+        inputs, pid, camid = _parse_data_for_eval(d)
+        input_img = inputs.to(device)
+        outputs = model(input_img)
+        # print(outputs.shape)
+        if args.feat_inference == 'after':
+
+            f1 = outputs.data.cpu()
+            # flip
+            inputs = inputs.index_select(
+                3, torch.arange(inputs.size(3) - 1, -1, -1))
+            input_img = inputs.to(device)
+            outputs = model(input_img)
+            f2 = outputs.data.cpu()
+
+        else:
+            f1 = outputs[-1].data.cpu()
+            # flip
+            inputs = inputs.index_select(
+                3, torch.arange(inputs.size(3) - 1, -1, -1))
+            input_img = inputs.to(device)
+            outputs = model(input_img)
+            f2 = outputs[-1].data.cpu()
+
+        ff = f1 + f2
+        if ff.dim() == 3:
+            fnorm = torch.norm(
+                ff, p=2, dim=1, keepdim=True)  # * np.sqrt(ff.shape[2])
+            ff = ff.div(fnorm.expand_as(ff))
+            ff = ff.view(ff.size(0), -1)
+            # ff = ff.view(ff.size(0), -1)
+            # fnorm = torch.norm(ff, p=2, dim=1, keepdim=True)
+            # ff = ff.div(fnorm.expand_as(ff))
+
+        else:
+            fnorm = torch.norm(ff, p=2, dim=1, keepdim=True)
+            ff = ff.div(fnorm.expand_as(ff))
+            # pass
+        # fnorm = torch.norm(ff, p=2, dim=1, keepdim=True)
+        # ff = ff.div(fnorm.expand_as(ff))
+        features = torch.cat((features, ff), 0)
+        pids.extend(pid)
+        camids.extend(camid)
+        # print(features.shape)
+    return features, np.asarray(pids), np.asarray(camids)
+
+
+def main():
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--root', type=str)
+    # parser.add_argument('-d', '--dataset', type=str, default='market1501')
+    # parser.add_argument('-m', '--model', type=str, default='osnet_x1_0')
+    # parser.add_argument('--weights', type=str)
+    # parser.add_argument('--save-dir', type=str, default='log')
+    # parser.add_argument('--height', type=int, default=256)
+    # parser.add_argument('--width', type=int, default=128)
+    # args = parser.parse_args()
+
+    if args.config != '':
+        with open(args.config, 'r') as f:
+            config = yaml.load(f)
+        for op in config:
+            setattr(args, op, config[op])
+
+    # loader = data.Data(args)
+    ckpt = utility.checkpoint(args)
+    loader = data_v2.ImageDataManager(args)
+    model = Model(args, ckpt)
+    optimzer = optim.make_optimizer(args, model)
+    # loss = loss.make_loss(args, ckpt) if not args.test_only else None
+
+    start = -1
+    if args.load != '':
+        start = ckpt.resume_from_checkpoint(
+            osp.join(ckpt.dir, 'model-latest.pth'), model, optimzer) - 1
+    if args.pre_train != '':
+        ckpt.load_pretrained_weights(model, args.pre_train)
+
+    scheduler = optim.make_scheduler(args, optimzer, start)
+
+    # print('[INFO] System infomation: \n {}'.format(get_pretty_env_info()))
+    ckpt.write_log('[INFO] Model parameters: {com[0]} flops: {com[1]}'.format(
+        com=compute_model_complexity(model, (1, 3, args.height, args.width))))
+
+    use_gpu = torch.cuda.is_available()
+
+    # datamanager = torchreid.data.ImageDataManager(
+    #     root=args.root,
+    #     sources=args.dataset,
+    #     height=args.height,
+    #     width=args.width,
+    #     batch_size_train=100,
+    #     batch_size_test=100,
+    #     transforms=None,
+    #     train_sampler='SequentialSampler'
+    # )
+    # test_loader = loader.testloader
+    test_loader = loader.test_loader
+    query_loader = loader.query_loader
+    query_dataset = loader.queryset.query
+    gallery_dataset = loader.galleryset.gallery
+    # model = torchreid.models.build_model(
+    #     name=args.model,
+    #     num_classes=datamanager.num_train_pids,
+    #     use_gpu=use_gpu
+    # )
+
+    if use_gpu:
+        model = model.cuda()
+
+    device = torch.device('cuda' if use_gpu else 'cpu')
+
+    model.eval()
+
+    # self.ckpt.add_log(torch.zeros(1, 6))
+    # qf = self.extract_feature(self.query_loader,self.args).numpy()
+    # gf = self.extract_feature(self.test_loader,self.args).numpy()
+    with torch.no_grad():
+
+        qf, query_ids, query_cams = extract_feature(
+            model, device, query_loader, args)
+        gf, gallery_ids, gallery_cams = extract_feature(
+            model, device, test_loader, args)
+
+    if args.re_rank:
+        q_g_dist = np.dot(qf, np.transpose(gf))
+        q_q_dist = np.dot(qf, np.transpose(qf))
+        g_g_dist = np.dot(gf, np.transpose(gf))
+        dist = re_ranking(q_g_dist, q_q_dist, g_g_dist)
+    else:
+        # dist = cdist(qf, gf,metric='cosine')
+
+        # cosine distance
+        dist =1- torch.mm(qf, gf.t()).cpu().numpy()
+
+    # if args.weights and check_isfile(args.weights):
+    #     load_pretrained_weights(model, args.weights)
+    save_dir = ckpt.dir
+
+    visualize_ranked_results(
+        dist,
+        query_dataset,
+        gallery_dataset,
+        loader.data_type,
+        width=loader.width,
+        height=loader.height,
+        save_dir=osp.join(save_dir, 'visrank'),
+        # topk=visrank_topk)
+    )
+
+
+if __name__ == '__main__':
+    main()
