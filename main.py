b7c68319b2f5459423fd2fd5c85ace5765da2546
Fabian Herzog <fabianfubel@gmail.com>
2024-06-18 20:48:12 +0200

    Update requirements to support recent versions.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import data_v2
 from loss import make_loss
 from model import make_model
 from optim import make_optimizer, make_scheduler
+
 # import engine_v1
 # import engine_v2
 import engine_v3
@@ -15,9 +16,9 @@ import yaml
 import torch
 
 
-if args.config != '':
-    with open(args.config, 'r') as f:
-        config = yaml.load(f)
+if args.config != "":
+    with open(args.config, "r") as f:
+        config = yaml.full_load(f)
     for op in config:
         setattr(args, op, config[op])
 torch.backends.cudnn.benchmark = True
@@ -30,26 +31,28 @@ optimzer = make_optimizer(args, model)
 loss = make_loss(args, ckpt) if not args.test_only else None
 
 start = -1
-if args.load != '':
+if args.load != "":
     start, model, optimizer = ckpt.resume_from_checkpoint(
-        osp.join(ckpt.dir, 'model-latest.pth'), model, optimzer)
+        osp.join(ckpt.dir, "model-latest.pth"), model, optimzer
+    )
     start = start - 1
-if args.pre_train != '':
+if args.pre_train != "":
     ckpt.load_pretrained_weights(model, args.pre_train)
 
 scheduler = make_scheduler(args, optimzer, start)
 
 # print('[INFO] System infomation: \n {}'.format(get_pretty_env_info()))
-ckpt.write_log('[INFO] Model parameters: {com[0]} flops: {com[1]}'.format(com=compute_model_complexity(model, (1, 3, args.height, args.width))
-                                                                          ))
+ckpt.write_log(
+    "[INFO] Model parameters: {com[0]} flops: {com[1]}".format(
+        com=compute_model_complexity(model, (1, 3, args.height, args.width))
+    )
+)
 
-engine = engine_v3.Engine(args, model, optimzer,
-                          scheduler, loss, loader, ckpt)
+engine = engine_v3.Engine(args, model, optimzer, scheduler, loss, loader, ckpt)
 # engine = engine.Engine(args, model, loss, loader, ckpt)
 
 n = start + 1
 while not engine.terminate():
-
     n += 1
     engine.train()
     if args.test_every != 0 and n % args.test_every == 0:
